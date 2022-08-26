b7197e9cd109fdafa0909aa5f40d36104804cacf
Affan2310 <affanrazaqadri2310@gmail.com>
2022-08-26 12:50:30 +0530

    affan

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Todo from './component/todoreact/todo';
+import Temp from './component/weather/temp';
+
+const App = () => {
+  return (
+    <>
+  <Temp />
+  
+
+  </>
+  );
+};
+
+export default App
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,96 @@
+const Menu = [
+    {
+      id: 1,
+      image: "https://images.mygoodtimes.in/wp-content/uploads/2018/08/31063759/iStock-643845180-1.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "https://2.bp.blogspot.com/-zaN1RnvuKYo/Vxkflp2GcYI/AAAAAAAADVc/dbR6GaD5kBAqdaN_EMcbPKqIDv2tgDtXQCLcB/s1600/kashmiri%2Bdum%2Baloo%2Bedit%2B2%2Bname%2B1.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https:%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2Fimage%2F2017%2F06%2Fmain%2Fgrilled-corn-salsa-1708p110_0.jpg%3Fitok%3D1bTZSySW%261527094421",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "https://i.pinimg.com/originals/fe/2a/8e/fe2a8e292005e55bf0d23df10d0875b9.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "https://pizzaneed.com/wp-content/uploads/2019/05/different-types-of-pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "https://festivalindia.com.au/wp-content/uploads/2020/05/non-veg-thali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "https://tse4.mm.bing.net/th?id=OIP.2wime2gtE9wDDcNZ4RxGCgHaE9&pid=Api&P=0",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "https://img.onmanorama.com/content/dam/mm/en/food/readers-recipe/images/2020/11/30/jeera-rice-rajma.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "https://im.whatshot.in/img/2020/Jun/istock-967110094-1-1593429197.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+
+    {
+      id: 10,
+      image: "https://tse3.mm.bing.net/th?id=OIP.oGVnUAZlKs3iSaQJkxRZLgHaFj&pid=Api&P=0",
+      name: "Burger",
+      category: "evening",
+      price: "15₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
